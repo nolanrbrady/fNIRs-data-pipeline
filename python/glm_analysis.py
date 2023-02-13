b08adf3d279ce8a26e31e2f91d95c09477a4d13e
Nolan Brady <nolanrbrady@gmail.com>
2023-02-13 13:03:17 -0700

    Generating and saving GLM model for each participant

---
+++ python/glm_analysis.py
@@ -0,0 +1,51 @@
+# Import common libraries
+import numpy as np
+import pandas as pd
+
+# Import MNE processing
+from mne.preprocessing.nirs import optical_density, beer_lambert_law
+
+# Import MNE-NIRS processing
+from mne_nirs.statistics import run_glm
+from mne_nirs.experimental_design import make_first_level_design_matrix
+from mne import Epochs, events_from_annotations
+from mne_nirs.statistics import statsmodels_to_results
+from mne_nirs.channels import get_short_channels, get_long_channels
+from mne_nirs.channels import picks_pair_to_idx
+from mne_nirs.visualisation import plot_glm_group_topo
+from mne_nirs.datasets import fnirs_motor_group
+from mne_nirs.visualisation import plot_glm_surface_projection
+from mne_nirs.io.fold import fold_channel_specificity
+
+# Import MNE-BIDS processing
+from mne_bids import BIDSPath, read_raw_bids, get_entity_vals
+
+# Import StatsModels
+import statsmodels.formula.api as smf
+
+# Import Plotting Library
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+
+def create_design_matrix(all_data):
+    updated_data = []
+    for data in all_data:
+        epoch, condition, raw_haemo = data.values()
+        print(raw_haemo)
+        events, event_dict = events_from_annotations(raw_haemo, verbose=False)
+        
+        for event in events:
+            # Dynamically establish the task length
+            prev_event_time = events[-2][0]
+            current_event_time = events[-1][0]
+            task_len = current_event_time - prev_event_time
+        print(task_len)
+        design_matrix = make_first_level_design_matrix(raw_haemo, stim_dur=task_len)
+        data['design_matrix'] = design_matrix
+        updated_data.append(data)
+        print(data)
+    
+    return updated_data
+        
