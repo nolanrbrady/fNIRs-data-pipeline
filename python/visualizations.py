4c8b024d1461f8382cfdd4bc6a213e9be147c488
Nolan Brady <nolanrbrady@gmail.com>
2023-03-24 17:20:41 -0600

    Removing bad channels from the topographical results

--- python/visualizations.py
+++ python/visualizations.py
@@ -36,7 +36,7 @@ from pprint import pprint
 
 
 
-def group_topological_visualisation(df_cha, columns_for_glm_contrast, raw_haemo, group):
+def group_topological_visualisation(df_cha, columns_for_glm_contrast, raw_haemo, group, fdr_correction=False):
     chromas = ['hbo', 'hbr']
     conditions = len(columns_for_glm_contrast)
     ratios = [1 for i in range(conditions)]
@@ -61,15 +61,53 @@ def group_topological_visualisation(df_cha, columns_for_glm_contrast, raw_haemo,
         ch_model_df = statsmodels_to_results(ch_model)
 
         ch_model_df['Coef.'] = ch_model_df['Coef.'] * 1e7 
-
         for condition_id, condition in enumerate(columns_for_glm_contrast):
             color_bar = True if condition_id + 1 == len(columns_for_glm_contrast) else False
+            #TODO: Seems like having a bad channel breaks the pathway here.
+            # model_data = ch_model_df.query(f"Condition in ['{condition}']").head(17)
+            model_data = ch_model_df.query(f"Condition in ['{condition}']")
+            bad_channels = raw_haemo.info['bads']
+
+            mask = ~model_data['ch_name'].isin(bad_channels)
+            model_data = model_data.loc[mask]
+            
             # Plot the condition
-            plot_glm_group_topo(raw_haemo.copy().pick(picks=chroma),
-                                ch_model_df.query(f"Condition in ['{condition}']"),
+            plot_glm_group_topo(raw_haemo.copy().pick(picks=chroma), model_data,
                                 colorbar=color_bar, axes=axes[chroma_id, condition_id],
                                 vlim=vlim, cmap=cmap_color, threshold=True)
     plt.show()
+
+def group_topological_visualisation_with_fdr(df_cha, raw_haemo):
+    chromas = ['hbo', 'hbr']
+
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 10),
+                         gridspec_kw=dict(width_ratios=[1]))
+    
+    fig.suptitle("FDR Adjusted", fontsize=20)
+    
+    for chroma_id, chroma in enumerate(chromas):
+        cmap_color = mpl.cm.Oranges if chroma == 'hbo' else mpl.cm.Blues_r
+        vlim = (0, 20) if chroma == 'hbo' else (-10, 0)
+
+        # Cut down the dataframe just to the conditions we are interested in
+        ch_summary = df_cha.query(f"Condition in 'Neutral'")
+        ch_summary = ch_summary.query(f"Chroma in ['{str(chroma)}']")
+
+        # Run group level model and convert to dataframe
+        ch_model = smf.mixedlm("theta ~ ch_name:Condition:Chroma",
+                            ch_summary, groups=ch_summary["ID"]).fit(method='nm')
+
+        ch_model_df = statsmodels_to_results(ch_model)
+        
+    return ch_model
+    #     ch_model_df['Coef.'] = ch_model_df['Coef.'] * 1e7 
+
+    #     # Plot the condition
+    #     plot_glm_group_topo(raw_haemo.copy().pick(picks=chroma),
+    #                         ch_model_df.query(f"Condition in 'Neutral'"),
+    #                         colorbar=True, axes=axes[chroma_id],
+    #                         vlim=vlim, cmap=cmap_color, threshold=True)
+    # plt.show()
 
 
 
@@ -85,43 +123,13 @@ def group_plot_visualisation(df_cha, columns_for_glm_contrast, raw_haemo):
     sns.catplot(x="Condition", y="Coef.", hue="Significant", data=df, ci=None, palette="muted", height=4, s=10)
 
 
-def group_cortical_surface_projection(df_cha, columns_for_glm_constrast, raw_haemo):
-    #TODO: This doesn't work. We need to fix it.
-    cha_summary = df_cha.query("Chroma in ['hbo']")
-
-    # Run group level model and convert to dataframe
-    con_model = smf.mixedlm("theta ~ -1 + ch_name:Chroma",
-                            cha_summary, groups=cha_summary["ID"]).fit(method='nm')
-    con_model_df = statsmodels_to_results(con_model,
-                                        order=raw_haemo.copy().pick(
-                                            picks="hbo").ch_names)
-    # Generate brain figure from data
-    clim = dict(kind='value', pos_lims=(0, 8, 11))
-    brain = plot_glm_surface_projection(raw_haemo.copy().pick("hbo"),
-                                        con_model_df, clim=clim, view='dorsal',
-                                        colorbar=True, size=(800, 700))
-    
-    brain.add_text(0.05, 0.95, "Left-Right", 'title', font_size=16, color='k')
-
-    # Run model code as above
-    clim = dict(kind='value', pos_lims=(0, 11.5, 17))
-    for idx, cond in enumerate(columns_for_glm_constrast):
-
-        # Run same model as explained in the sections above
-        ch_summary = df_cha.query("Condition in [@cond]")
-        ch_summary = ch_summary.query("Chroma in ['hbo']")
-        ch_model = smf.mixedlm("theta ~ -1 + ch_name", ch_summary,
-                            groups=ch_summary["ID"]).fit(method='nm')
-        model_df = statsmodels_to_results(ch_model, order=raw_haemo.copy().pick("hbo").ch_names)
-
-        # Generate brain figure from data
-        brain = plot_glm_surface_projection(raw_haemo.copy().pick("hbo"),
-                                            model_df, clim=clim, view='dorsal',
-                                            colorbar=True, size=(800, 700))
-        brain.add_text(0.05, 0.95, cond, 'title', font_size=16, color='k')
+def group_cortical_surface_projection(df_cha, columns_for_glm_constrast, raw_haemo, path):
+    brain = mne.viz.Brain('fsaverage', subjects_dir=path, background='w', cortex='0.5')
+    brain.add_sensors(raw_haemo.info, trans='fsaverage', fnirs=['channels', 'pairs', 'sources', 'detectors'])
+    brain.show_view(azimuth=180, elevation=80, distance=450)
 
 
-def plot_waveform_analysis(all_evokeds, variable_epoch_time):
+def plot_waveform_analysis(all_evokeds, interval_length, variable_epoch_time):
     if variable_epoch_time:
         return RuntimeError('Waveform Analysis is not possible with variable length tasks')
     else:
@@ -131,7 +139,7 @@ def plot_waveform_analysis(all_evokeds, variable_epoch_time):
 
         for (pick, color) in zip(['hbo', 'hbr'], ['r', 'b']):
             for idx, evoked in enumerate(all_evokeds):
-                print(evoked, len(all_evokeds[evoked]))
+                # print(evoked, len(all_evokeds[evoked]))
                 plot_compare_evokeds({evoked: all_evokeds[evoked]}, combine='mean',
                                     picks=pick, axes=axes[idx], show=False,
                                     colors=[color], legend=False, ylim=lims, ci=0.95,
